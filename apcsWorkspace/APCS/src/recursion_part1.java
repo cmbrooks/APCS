2c443eb37220d0361e046e7e9d3e92d2c4ba52d6
cmbrooks <codymb36@gmail.com>
2014-09-25 15:28:12 -0500

    Finished testing Ackermann

--- apcsWorkspace/APCS/src/recursion_part1.java
+++ apcsWorkspace/APCS/src/recursion_part1.java
@@ -6,25 +6,48 @@ public class recursion_part1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(fact(6));
+		System.out.println("Factoral of 6: " + fact(6));
+		System.out.println("Fibonacci of 8: " + fib(8));
+		System.out.println("Ackermann of 2 and 3: " + ack(2, 3));
 
 	}
 	
 
   	public static int fact(int n){
-		int nfact;
+		int result;
 		if (n == 0){
-			nfact = 1;
-			return nfact;
+			result = 1;
+			return result;
 		}else{
-			nfact = n*fact(n-1);
+			result = n*fact(n-1);
 		}
-		return nfact;
+		return result;
 	}
 
+  	
+  	public static int fib(int x){
+  		int result;
+  		if (x == 0){
+  			return 0;
+  		}else if(x == 1){
+  			return 1;
+  		}else{
+  			result = fib(x-1) + fib(x-2);
+  		}
+  		return result;
+  	}
+  	
 	
-	public static void ack (int m, int n){
-		
+	public static int ack(int m, int n){
+		int result;
+		if(m == 0){
+			result = n+1;
+		}else if(m > 0 && n == 0){
+			result = ack((m-1), 1);
+		}else{
+			result = ack(m-1, ack(m, n-1));
+		}
+		return result;
 	}
 
 }
