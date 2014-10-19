75297cf554aa2f6c30819afff3b88ec1df01fc92
Cody <codymb36@gmail.com>
2014-10-19 17:05:07 -0500

    Markning multiples of p works; does not move to the next value of p

--- apcsWorkspace/APCS/src/sieve_of_eratosthenes.java
+++ apcsWorkspace/APCS/src/sieve_of_eratosthenes.java
@@ -4,32 +4,35 @@ public class sieve_of_eratosthenes {
 	public static void main(String[] args) {
 
 		int largestNumber = 100;
-		int arraySize = largestNumber - 1;
-		int[] numberList = new int[arraySize];
+		boolean[] numberStatus = new boolean[largestNumber+1];
 		int p = 2;
-
 		
-		//Fill array with numbers from 0 to the array size
-		for(int i = 2; i <= arraySize+1; i++){
-			numberList[i-2] = i;
+		//Initialize array as all numbers are prime (true)
+		for(int i = 0; i <= numberStatus.length-1; i++){
+			numberStatus[i] = true;
 		}
 		
-		//Remove non-prime numbers
-		for(int i = 2*p; i <= arraySize+1; i = p+i){
+		
+		for(int i = 2*p; i <= numberStatus.length; i = p+i){
 			System.out.println("Remove: " + i);
-			for(int j = i-1; j <= numberList.length - 1; j+=2){
-				numberList[j-1] = numberList[j];
-			}
-
-//			nPrimeList[nprimeIndex] = i;
-//			nprimeIndex++;
+			numberStatus[i-2] = false;
 		}
+
 		
 		//Print all elements in array
+//		System.out.println("");
+//		System.out.println("Final Array:");
+//		for(int i = 0; i <= numberStatus.length-1; i++){
+//			System.out.println(i + ": " + numberStatus[i]);
+//		}
+		
+		//Print all prime numbers in range
 		System.out.println("");
-		System.out.println("Final Array:");
-		for(int i = 0; i <= numberList.length - 1; i++){
-			System.out.println(numberList[i]);
+		System.out.println("Prime Numbers:");
+		for(int i = 0; i <= numberStatus.length-1; i++){
+			if(numberStatus[i] == true){
+				System.out.println(i);
+			}
 		}
 		
 	}
--- apcsWorkspace/APCS/src/test.java
+++ apcsWorkspace/APCS/src/test.java
@@ -1,19 +1,31 @@
-
-public class test {
-
-	public static void main(String[] args) {
-		int[] testArray = {1,2,3,4,5,6,7,8,9,10};
-		int i = 2;
+public class test{//Declare the array
+	public static void main(String[] args){
 		
-		for(int j = i; j <= testArray.length - 1; j++){
-			testArray[j-1] = testArray[j];
+		boolean numbers [] = new boolean[100];
+		int y = 0;
+		
+		//Declare all numbers as true to begin
+		for(int i = 2; i < 100;i++){
+		    numbers[i] = true;
+		}
+		//Run loop that increases i and multiplies it by increasing multiples
+		for (int x = 2; x < 100; x++) {
+		
+		    //A loop for the increasing multiples; keep those numbers below 100
+		    //Set any multiple of "x" to false
+		    for(int n = 2; y < 100; n++){
+			    y = n * x;
+			    if(y<100){
+			    	numbers[y] = false;
+			    }
+		    }
 		}
 		
-		//Print all elements in numberList
-		for(int k = 0; i <= testArray.length - 1; k++){
-			System.out.println(testArray[i]);
+		//Print all elements in array
+		System.out.println("");
+		System.out.println("Final Array:");
+		for(int i = 0; i <= numbers.length-1; i++){
+			System.out.println(i + ": " + numbers[i]);
 		}
-
 	}
-
 }
