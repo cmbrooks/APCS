4cfaff826672e7ba020e8bb7d1ee38c4921fe806
unknown <cobrooks@HS-Skogman-8874.linnmar.local>
2014-10-22 08:11:48 -0500

    Finished Sieves program

--- apcsWorkspace/APCS/src/sieve_of_eratosthenes.java
+++ apcsWorkspace/APCS/src/sieve_of_eratosthenes.java
@@ -1,4 +1,6 @@
-
+/*Exercise 2.3 2014-09-15
+ *Cody Brooks
+ */
 public class sieve_of_eratosthenes {
 
 	public static void main(String[] args) {
@@ -6,31 +8,25 @@ public class sieve_of_eratosthenes {
 		int largestNumber = 100;
 		boolean[] numberStatus = new boolean[largestNumber+1];
 		int p = 1;
-		
+
 		//Initialize array as all numbers are prime (true)
 		for(int i = 0; i <= numberStatus.length-1; i++){
 			numberStatus[i] = true;
 		}
-		
+
 		//Find the next value of p (The next true index)
-		for(int j = p+1; j <= numberStatus.length; j++){
-			if(numberStatus[j]){
+		for(int j = p+1; j < numberStatus.length; j++){
+			if(numberStatus[j] == true){
 				p = j;
+				System.out.println("Current P: " + p);
 				//Mark all multiples of p as false
 				for(int i = 2*p; i <= numberStatus.length; i = p+i){
 					System.out.println("Remove: " + i);
-					numberStatus[i-2] = false;
+					numberStatus[i] = false;
 				}
 			}
 		}
 
-		//Print all elements in array
-//		System.out.println("");
-//		System.out.println("Final Array:");
-//		for(int i = 0; i <= numberStatus.length-1; i++){
-//			System.out.println(i + ": " + numberStatus[i]);
-//		}
-		
 		//Print all prime numbers in range
 		System.out.println("");
 		System.out.println("Prime Numbers:");
@@ -39,7 +35,7 @@ public class sieve_of_eratosthenes {
 				System.out.println(i);
 			}
 		}
-		
+
 	}
 
 }
