67d21250babfcc578436711195dbd32ea6352a4b
unknown <cobrooks@HS-MC-8758.linnmar.local>
2014-11-13 09:10:58 -0600

    Added lots of functionality to matrixmath.java; All methods except matrix multiplication are finished

--- apcsWorkspace/APCS/src/matrixmath.java
+++ apcsWorkspace/APCS/src/matrixmath.java
@@ -6,90 +6,187 @@ Cody Brooks
 public class matrixmath {
 
   public static void main (String[] args){
-	  int[][] A = {
+	  /*Setup inputs*/
+    int[][] A = {
+			  		  {2,5,7},
+					  {7,3,9},
+					  {7,2,8}
+	  			  };
+	  int[][] B = {
 			  		  {2,5,7},
 					  {7,3,9},
 					  {7,2,8}
 	  			  };
 	  int p = 2;
-	  System.out.println("Scalar addition of A + p" + MatMath.add(A, p));
+
+	  int[][] resultArray = MatMath.mult(A, B);
+
+	  /*Print the result array*/
+	  System.out.println("Multiplication of A + B:");
+	  for (int i = 0; i < resultArray.length; i++) {
+		  for (int j = 0; j < resultArray[0].length; j++) {
+			  System.out.print("\t" + resultArray[i][j]);
+		  }
+		  System.out.println();
+	  }
+
   }
 
 }
 
 class MatMath {
 
-  /*Scalar Addition*/
-  static int [][] add (int p, int[][] A){
-    int[][] result = new int[A.length][A[0].length];
-
-    for (int i = 0; i < A.length; i++) {
-      for (int j = 0; j < A[i].length; j++) {
-        result[i][j] = p + A[i][j];
-      }
-    }
+	/*Scalar Addition*/
+	static int [][] add (int p, int[][] A){
+	    int[][] result = new int[A.length][A[0].length];
+		if (checkMat(A)){
+		    for (int i = 0; i < A.length; i++) {
+		      for (int j = 0; j < A[i].length; j++) {
+		        result[i][j] = p + A[i][j];
+		      }
+		    }
+		} else {
+			System.out.println("The input A is not a valid matrix");
+			//Throw an exception
+		}
 
     return result;
-  }
+	}
 
   /*Scalar Addition*/
   static int [][] add (int[][] A, int p){
     int[][] result = new int[A.length][A[0].length];
+	if (checkMat(A)){
+	    for (int i = 0; i < A.length; i++) {
+	      for (int j = 0; j < A[i].length; j++) {
+	        result[i][j] = p + A[i][j];
+	      }
+	    }
+	} else {
+		System.out.println("The input A is not a valid matrix");
+		//Throw an exception
+	}
 
-    for (int i = 0; i < A.length; i++) {
-      for (int j = 0; j < A[i].length; j++) {
-        result[i][j] = p + A[i][j];
-      }
-    }
-
-    return result;
+	return result;
   }
 
-  /*Scalar Multiplicaion*/
+  /*Scalar Multiplication*/
   static int [][] mult (int p, int[][] A){
     int[][] result = new int[A.length][A[0].length];
+	if (checkMat(A)){
+	    for (int i = 0; i < A.length; i++) {
+	      for (int j = 0; j < A[i].length; j++) {
+	        result[i][j] = p * A[i][j];
+	      }
+	    }
+	} else {
+		System.out.println("The input A is not a valid matrix");
+		//Throw an exception
+	}
 
-    for (int i = 0; i < A.length; i++) {
-      for (int j = 0; j < A[i].length; j++) {
-        result[i][j] = p * A[i][j];
-      }
-    }
-
-    return result;
+	return result;
   }
 
-  /*Scalar Multiplicaiton*/
+  /*Scalar Multiplication*/
   static int [][] mult (int[][] A, int p){
-    int[][] result = new int[A.length][A[0].length];
+	if (checkMat(A)){
+	    int[][] result = new int[A.length][A[0].length];
+	    for (int i = 0; i < A.length; i++) {
+	      for (int j = 0; j < A[i].length; j++) {
+	        result[i][j] = p * A[i][j];
+	      }
+	    }
+	    return result;
+	} else {
+		System.out.println("The input A is not a valid matrix");
+		//Throw an exception
+	}
 
-    for (int i = 0; i < A.length; i++) {
-      for (int j = 0; j < A[i].length; j++) {
-        result[i][j] = p * A[i][j];
-      }
-    }
-
-    return result;
+	return null;
   }
 
   /*Matrix Addition*/
   static int [][] add (int[][] A, int[][] B){
-	int[][] result = new int[A[0].length][B.length];
-    for (int i = 0; i < A.length; i++) {
-        for (int j = 0; j < A[i].length; j++) {
-          /*Perform Matrix multiplication here*/
-        }
-      }
+	  int[][] result = new int[ A[0].length ][ B.length ];
+	  if (sameSize(A,B)){
+	    for (int i = 0; i < A.length; i++) {
+	        for (int j = 0; j < A[i].length; j++) {
+	          result[i][j] = A[i][j] + B[i][j];
+	        }
+	    }
+	  } else {
+		System.out.println("A and B cannot be added together");
+		//Throw exception
+	  }
+
 	  return result;
   }
 
   /*Matrix Multiplication*/
   static int [][] mult (int[][] A, int[][] B){
-	int[][] result = new int[A[0].length][B.length];
-    for (int i = 0; i < A.length; i++) {
-        for (int j = 0; j < A[i].length; j++) {
-          /*Perform Matrix addition here*/
-        }
+  	if ( checkMat(A) && checkMat(b) && canMult(A, B) ) {
+      int[][] result = new int[ A[0].length ][ B.length ];
+    	int multSum = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+            	for (int k = 0; k < A[0].length; k++) {
+                multSum += ( A[i][k] * B[k][j] );
+              }
+              result[i][j] = multSum;
+            }
+          }
+      } else {
+        Stystem.out.println("A and B cannot be multiplied");
       }
+	  return result;
+  }
+
+  /*Ensures that the given matrix has a valid size*/
+  static boolean checkMat (int [][] A) {
+
+	  boolean result = false;
+
+	  for (int i = 0; i < A.length; i++) {
+		  if (A[i].length == A[0].length) {
+			  /*Matrix is valid*/
+			  result = true;
+		  } else {
+			  /*Matrix is invalid*/
+			  result = false;
+			  break;
+		  }
+	  }
+
+	  if (result) {
+		  System.out.println("Matrix is valid");
+	  } else {
+		  System.out.println("Matrix is invalid");
+	  }
+
+	  return result;
+  }
+
+  /*Checks to see if two matrices are the same dimensions*/
+  static boolean sameSize (int[][] A, int[][] B) {
+	  boolean result = false;
+	  if (checkMat(A) && checkMat(B)) {
+		  if (A.length == B.length && A[0].length == B[0].length) {
+			  result = true;
+		  }
+	  } else {
+		  result = false;
+	  }
+	  return result;
+  }
+
+  /*Checks to see if two matrices are able to be multiplied*/
+  static boolean canMult (int[][] A, int[][] B) {
+	  boolean result = false;
+	  if (checkMat(A) && checkMat(B) && A[0].length == B.length) {
+		  result = true;
+	  } else {
+		  result = false;
+	  }
 	  return result;
   }
 
