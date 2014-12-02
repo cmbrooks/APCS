ab8a9c625eb3dfed577c99c655ace3fe66f9f2ee
Cody <codymb36@gmail.com>
2014-12-02 14:16:26 +0000

    Finished select sorting

--- newWorkspace/apcsProject/src/sorting/select.java
+++ newWorkspace/apcsProject/src/sorting/select.java
@@ -2,27 +2,25 @@ public class select {
     
     public static void main (String[] args) {
         
-        int[] sortArray = {3,2,4,1};
-        int maxNumber = sortArray[0];
+        int[] sortArray = {3,6,7,9,1,2,4,5};
         int maxIndex = -1;
         int temp = -1;
+        int unsortedLen = sortArray.length - 1;
         
         /*Sort array using select sorting*/
-        for (int i = 0; i < sortArray.length; i++) {
-            
-            //compNumber = sortArray[i];
+        for (int i = sortArray.length - 1; i > 1; i--) {
+            maxIndex = 0;
             System.out.println("Outer for loop itteration: " + i);
-            for (int j = 1; j < sortArray.length; j++) {
+            for (int j = 1; j < unsortedLen; j++) {
                 System.out.println("Inner For loop itteration: " + j);
-                System.out.println("Number being compared: " + maxNumber);
+                System.out.println("Number being compared: " + sortArray[maxIndex]);
                 System.out.println("Element at index j: " + sortArray[j]);
-                /**HERE IS THE PROBLEM!!!**/
-                if (maxNumber < sortArray[j]) {
-                    System.out.println(maxNumber + " is less than " + sortArray[i] + " so new max number is " + sortArray[i]);
-                    maxNumber = sortArray[j];
+                if (sortArray[maxIndex] < sortArray[j]) {
+                    System.out.print(sortArray[maxIndex] + " is less than " + sortArray[j]);
                     maxIndex = j;
+                    System.out.println(" so new max number is " + sortArray[maxIndex]);
                 } else {
-                    System.out.println("Max is still max. Do nothing");
+                    System.out.println(sortArray[maxIndex] + " is greater than " + sortArray[j] + ". Do nothing");
                 }
                 System.out.println("//////////////");
                 
@@ -31,8 +29,9 @@ public class select {
             temp = sortArray[i];
             sortArray[i] = sortArray[maxIndex];
             sortArray[maxIndex] = temp;
-            //System.out.println(sortArray[i] + " was swapped with " + sortArray[maxIndex]);
+            System.out.println(sortArray[i] + " was swapped with " + sortArray[maxIndex]);
             System.out.println();
+            unsortedLen--;
         }
         
         /*Print sorted array*/
